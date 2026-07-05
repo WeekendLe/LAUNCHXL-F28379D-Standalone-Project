ea3fc7ffabf0365a2cebaa4ef3b4b8cc5fc7dde4
WeekendLe <106853709+WeekendLe@users.noreply.github.com>
2026-07-05 10:37:36 +0800

    Add files via upload

---
+++ include/project.h
@@ -0,0 +1,35 @@
+/*
+ *  project.h
+ *  Including all headers file
+ *  Declare any function prototypes
+ * 
+ *  Created on: June 24, 2026
+ *      Author: Le-Nguyen Anh-Tuan
+ */
+
+#ifndef PROJECT_H_
+#define PROJECT_H_
+
+// C library
+#include <stdint.h>
+#include <stdbool.h>
+
+// F28379D
+#include "F2837xD_device.h"
+#include "F2837xD_Examples.h"
+
+// Custome files
+#include "SciDriverF28379D.h"
+
+// Device library
+
+// Function Prototype
+void vHardwareSetup(void);
+void vSciaTransmit(void);
+
+// Interrupt Handler
+interrupt void vEPWM1_InterruptHandler(void);
+interrupt void vSCIA_RX_InterruptHandler(void);
+interrupt void vCpuTimer1_InterruptHandler(void);
+
+#endif /* PROJECT_H_ */
